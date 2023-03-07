7c1ceccd8af1ab1d9bb03356f74f933eeef2a49d
mars <duvei@qq.com>
2023-03-07 21:57:10 +0800

    part 5

---
+++ 5_ledc_bsp/bsp/clk/bsp_clk.h
@@ -0,0 +1,5 @@
+#ifndef __BSP_CLK_H
+#define __BSP_CLK_H
+#include "imx6ul.h"
+void clk_enable(void);
+#endif
---
+++ 5_ledc_bsp/project/main.c
@@ -0,0 +1,20 @@
+#include "bsp_clk.h"
+#include "bsp_delay.h"
+#include "bsp_led.h"
+
+int main(void)
+{
+    clk_enable();
+    led_init();
+
+    while(1)
+    {
+        led_switch(LED0,ON);
+        delay(500);
+
+        led_switch(LED0,OFF);
+        delay(500);
+    }
+
+    return 0;
+}
